c613dfc3400dad03a89db814b2db7310cb07cd6c
sravanavaidehi <sravanavaidehi@gmail.com>
2024-10-17 12:06:22 -0500

    Update code.sv

--- code.sv
+++ code.sv
@@ -4,21 +4,19 @@ class ex;
   constraint No_Consecutive_Repetitions{
    foreach(ssn[i]){
      if(i>1){
-       !(ssn[i] == ssn[i-1] && ssn[i] == ssn[i-2]);
+       !(ssn[i] == ssn[i-1] && ssn[i] == ssn[i-2]);  // Ensure that ssn[i] is not equal to the previous two values (no three consecutive repetitions)
    }}}
    
    constraint No_Three_Numbers_Consecutive{
      foreach(ssn[i]){
        if(i>1){ 
-         !(ssn[i] == (ssn[i-1] + 1) && ssn[i-1] == (ssn[i-2] + 1));
+         !(ssn[i] == (ssn[i-1] + 1) && ssn[i-1] == (ssn[i-2] + 1)); // Prevent three consecutive increasing numbers (e.g., 1, 2, 3)
        }
      }
    }
    constraint No_Zero_Start{
-       ssn[0] != 0;
+     ssn[0] != 0; // Ensure that the first number in the SSN is not 0
      }
-
-   
       
  function void display();
         $write("SSN number : SS#");
